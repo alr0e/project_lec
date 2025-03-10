1314929edd4111a92f4bbe8d4cf49e552539a72c
alr0e <alreemali135@gmail.com>
2025-03-10 14:19:25 +0300

    update

---
+++ user.js
@@ -0,0 +1,32 @@
+[
+    {
+      "name": "Alreem",
+      "surname": "Ali",
+      "username": "Alreem_Ali",
+      "password": "password123",
+      "completed courses": [{
+        "courseId": "350",
+        "coursesName": "Web Development",
+        "grade": "90"
+      }]
+    },
+    {
+        "name": "Aysha",
+        "surname": "Hamad",
+        "username": "Aysha_Hamad",
+        "password": "password124",
+        "completed courses": []
+    },
+    {
+        "name": "Sara",
+        "surname": "Saleh",
+        "username": "Sara_Saleh",
+        "password": "password125",
+        "completed courses": [{
+            "courseId": "110",
+            "coursesName": "Social Work",
+            "grade": "80"
+    }]
+    },
+        
+]
